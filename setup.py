787f89fea23b61bdb70a014a206cf18bf170c612
ilay menahem <110230889+IlayMenahem@users.noreply.github.com>
2025-05-11 19:58:11 +0300

    make into a package

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="grobnerRl",
+    version="0.1.0",
+    author="ilay menahem",
+    author_email="ilay.menahem@campus.technion.ac.il",
+    description="A package for Grobner basis computation using reinforcement learning",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ilaymenahem/grobnerRl",
+    packages=find_packages(),
+    install_requires=[
+        'jax',
+        'chex',
+        'equinox',
+        'optax',
+        'gymnasium',
+        'tqdm',
+        'numpy',
+        'matplotlib',
+        'sympy',
+        'scipy'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
