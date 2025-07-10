ee7d01daf73f53c66782ba57839a602abc51ca3f
ilay menahem <110230889+IlayMenahem@users.noreply.github.com>
2025-07-10 18:07:02 +0300

    Refactor BC training to add validation and modularize logic
    - Replace train_bc with train_model supporting validation - Add
    accuracy_fn, train_epoch, and validate_model helpers - Update script to
    use new train_model and dataloader split

--- scripts/train_bc.py
+++ scripts/train_bc.py
@@ -2,8 +2,8 @@ import os
 import torch
 from torch.utils.data import DataLoader
 from grobnerRl.data import BCDataset, bc_collate
-from grobnerRl.models import GrobnerPolicy, Extractor, TwinExtractor, TwinPolicy
-from grobnerRl.rl.bc import train_bc
+from grobnerRl.models import GrobnerPolicy, Extractor
+from grobnerRl.rl.bc import train_model, bc_loss, accuracy_fn
 
 
 if __name__ == "__main__":
@@ -14,11 +14,13 @@ if __name__ == "__main__":
     path = os.path.join(os.getcwd(), 'data', 'optimal_reductions.json')
 
     dataset = BCDataset(path)
-    dataloader = DataLoader(dataset, batch_size=128, shuffle=True, collate_fn=bc_collate)
+    dataloader_train = DataLoader(dataset, batch_size=128, shuffle=True, collate_fn=bc_collate)
+    dataloader_val = DataLoader(dataset, batch_size=128, shuffle=False, collate_fn=bc_collate)
     policy = GrobnerPolicy(Extractor(3, 32, 128, 2, 4))
     # policy = TwinPolicy(TwinExtractor(num_vars, num_monomials, 256))
 
     optimizer = torch.optim.Adam(policy.parameters(), lr=5e-5)
     schedualer = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.5, patience=25)
 
-    policy = train_bc(policy, dataloader, 500, optimizer, schedualer)
+    policy = train_model(policy, dataloader_train, dataloader_val, 500, optimizer,
+        bc_loss, accuracy_fn, scheduler=schedualer)
